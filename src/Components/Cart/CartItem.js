d2903d2ea939ba23639494e223744320f51dc1ae
valentinazhbanova <112694404+valentinazhbanova@users.noreply.github.com>
2023-03-28 10:28:22 +0300

    Update CartItem.js

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { removeItemFromCart } from "../../redux/cartSlice";
 
 const CartItem = ({cartItem}) => {
-    console.log(cartItem)
     const dishes = dataDishes.find(item => item.id === cartItem.dishId);
     const dispatch = useDispatch();
 
